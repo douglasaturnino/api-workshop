5828c6c7c7000c80588f8f7169f887cde35b4958
Douglas Saturnino <95532957+douglasaturnino@users.noreply.github.com>
2025-02-07 11:58:44 -0300

    feat: adiciona a rota para buscar um produto (#3)

--- testes.py
+++ testes.py
@@ -23,3 +23,18 @@ def test_listar_produtos():
 def test_tamanho_lista_produtos():
     response = client.get("/produtos")
     assert len(response.json()) == 3
+
+
+def test_pega_um_produto():
+    response = client.get("/produtos/1")
+    assert response.status_code == 200
+
+
+def test_pega_um_produto_json():
+    response = client.get("/produtos/1")
+    assert response.json() == {
+        "id": 1,
+        "nome": "Smartphone",
+        "descricao": "Um smartphone de última geração",
+        "preco": 1500.00,
+    }
